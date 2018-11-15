8ac2756f8e52f22ffe8a6132161d7f33aee92498
Cheng-Yu Pai <mail4charliepai@gmail.com>
2018-11-14 23:16:16 -0800

    Random

--- osu_jukebox.py
+++ osu_jukebox.py
@@ -1,24 +1,50 @@
 #!/usr/bin/env python3
 
+import readline # enables history, etc. in input()
 import os
 import subprocess
 import shlex
+import random
 
 osu_dir = "/home/cpai/Games/Games-hdd/osu/osu-dir/Songs"
 
 def play(audio_filename):
-    return subprocess.Popen(["mpv", audio_filename])
+    return subprocess.run(["mpv", audio_filename], encoding="UTF-8")
 
+def play_dir(song_dir):
+    audio_filenames = list(filter(lambda d: "mp3" in d, os.listdir(osu_dir + "/" + song_dir)))
+    if len(audio_filenames) > 0:
+        sp = play(osu_dir + "/" + song_dir + "/" + audio_filenames[0])
+
+song_history = ""
 cmd = ""
 
 while True:
     cmd = input(">> ")
     tokens = shlex.split(cmd)
     f = tokens[0]
-    if f == "quit":
+    if f == "q":
         exit(0)
-    elif f == "test":
-        sp = play(osu_dir + "/399151 Camellia - crystallized/crystallized.mp3")
-        sp.wait()
     elif f == "rg":
         subprocess.run("ls " + osu_dir + " | " + cmd, shell=True)
+    elif f == "p":
+        song_dirs = list(filter(lambda d: d.split(" ")[0] == tokens[1], os.listdir(osu_dir)))
+        if len(song_dirs) > 0:
+            play_dir(song_dirs[0])
+            song_history = song_dirs[0]
+    elif f == "pr":
+        song_dirs = os.listdir(osu_dir)
+        random_dir = random.choice(song_dirs)
+        play_dir(random_dir)
+        song_history = random_dir
+    elif f == "lpr":
+        try:
+            while True:
+                song_dirs = os.listdir(osu_dir)
+                random_dir = random.choice(song_dirs)
+                play_dir(random_dir)
+                song_history = random_dir
+        except:
+            pass
+    elif f == "a":
+        play_dir(song_history)
